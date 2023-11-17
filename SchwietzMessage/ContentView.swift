daa54968e6cad9d549747fe44332a080e66725eb
Jan Phillip Schwietzer <schwietzer.jan@gmail.com>
2023-11-17 13:45:57 +0100

    Initial Commit

---
+++ SchwietzMessage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SchwietzMessage
+//
+//  Created by Jan Schwietzer on 17.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SchwietzMessage/SchwietzMessageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchwietzMessageApp.swift
+//  SchwietzMessage
+//
+//  Created by Jan Schwietzer on 17.11.23.
+//
+
+import SwiftUI
+
+@main
+struct SchwietzMessageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
